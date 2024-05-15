7bef4b86634d54385473068f6c6ea44151f97d49
Cody Parker <codywparker96@gmail.com>
2024-05-14 23:41:42 -0500

    removing unused functions

--- tracker/users.py
+++ tracker/users.py
@@ -1,5 +1,4 @@
 from dotenv import load_dotenv
-from auth import fetch_token
 import websockets
 import json
 import requests
@@ -15,37 +14,6 @@ async def init_socket():
         message = await websocket.recv()
         recv_message = json.loads(message)
         return recv_message['metadata']['message_id']
-    
-# def set_subscribe(message_id: str, token: str, user: dict):
-#     headers = {
-#         "Authorization": f"Bearer {token}",
-#         "Client-Id": client_id,
-#         "Content-Type": "application/json"
-#     }
-
-#     sub_data = {
-#         "type": "stream.online",
-#         "version": "1",
-#         "condition": {
-#             "broadcaster_id": user['data'][0]['id']
-#         },
-#         "transport": {
-#             "method": "websocket"
-#         }
-
-#     }
-
-#     subscribe = requests.post(url=stream_url + "/eventsub/subscriptions")
-
-# def get_users(token: str, streamer_list: list) -> dict:
-#     headers = {
-#         "Authorization": f"Bearer {token}",
-#         "Client-Id": client_id,
-#     }
-
-#     for streamer in streamer_list:
-#         users = requests.get(url=stream_url + f"/users?login={streamer}", headers=headers)
-#         return users.json()
 
 def get_streams(token: str):
     headers = {
