441934e0ab8d450ead4862dfad4c0e157138d943
Cody Parker <codywparker96@gmail.com>
2024-05-14 23:40:33 -0500

    removing unused cookie dict

--- tracker/drivers.py
+++ tracker/drivers.py
@@ -56,31 +56,6 @@ def init_driver() -> webdriver:
         "server_session_id": server_session_id
     }
 
-    # cookie_dict = {
-    #     "_ga": "GA1.1.394826768.1705724992",
-    #     "_ga_60KWEWG403": "GS1.1.1715740598.7.1.1715741383.60.0.0",
-    #     "_ga_NQNWDWJXCP": "GS1.1.1705902693.3.1.1705903138.0.0.0",
-    #     "_ga_RZGH9Y6L73": "GS1.1.1705901858.1.1.1705902107.0.0.0",
-    #     "_gid": "GA1.2.1639408864.1715738298",
-    #     "api_token": "e01664f1a555160feb96e6260d7127e8",
-    #     "auth-token": "zc18leuxijzpqfb69wox9tmbirkdjb",
-    #     "experiment-overrides": "{%22experiments%22:{}%2C%22disabled%22:[]}",
-    #     "language": "en",
-    #     "last_login": "2024-01-20T04:30:31Z",
-    #     "login": "a5yncio",
-    #     "name": "a5yncio",
-    #     "persistent": "432482890%3A%3Azjljamccjzyjd3sxdzrwza76nj4n1v",
-    #     "proto": "HTTP/2+alt",
-    #     "server_session_id": "a956b862341b49178f400d7be3c62289",
-    #     "tachyon-perfers-color-scheme": "dark-preferred",
-    #     "tachyon-user": "%7B%22id%22%3A%22432482890%22%2C%22login%22%3A%22a5yncio%22%2C%22version%22%3A%221%22%7D",
-    #     "twilight-user": "{%22authToken%22:%22zc18leuxijzpqfb69wox9tmbirkdjb%22%2C%22displayName%22:%22a5yncio%22%2C%22id%22:%22432482890%22%2C%22login%22:%22a5yncio%22%2C%22roles%22:{%22isStaff%22:false}%2C%22version%22:2}",
-    #     "twilight-user.dev": "{%22authToken%22:%22gcc6breqvl6v79px8k8rm087jg17d7%22%2C%22displayName%22:%22a5yncio%22%2C%22id%22:%22432482890%22%2C%22login%22:%22a5yncio%22%2C%22roles%22:{%22isStaff%22:false}%2C%22version%22:2}",
-    #     "twitch.lohp.countryCode": "US",
-    #     "unique_id": "4e96dba134c063d4",
-    #     "unique_id_durable": "4e96dba134c063d4"
-    # }
-
     for cookie in cookies:
         driver.add_cookie({"name": cookie, "value": cookies[cookie]})
     
